d33f814caceaa8619eb2bac956750c962d171b5a
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-03-11 10:31:20 +0100

    initial commit

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,33 +1,31 @@
 import { FiHeart } from "react-icons/fi";
 import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
-
 import "./Nav.css";
 
-const Nav = () => {
+const Nav = ({ handleInputChange, query }) => {
   return (
-    <Nav>
+    <nav>
       <div className="nav-container">
         <input
+          className="search-input"
           type="text"
-          className="search.input"
+          onChange={handleInputChange}
+          value={query}
           placeholder="Enter your search shoes."
         />
       </div>
-
       <div className="profile-container">
-        <a href="">
+        <a href="#">
           <FiHeart className="nav-icons" />
         </a>
-
         <a href="">
           <AiOutlineShoppingCart className="nav-icons" />
         </a>
-
         <a href="">
           <AiOutlineUserAdd className="nav-icons" />
         </a>
       </div>
-    </Nav>
+    </nav>
   );
 };
 
